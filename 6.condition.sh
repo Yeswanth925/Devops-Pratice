ad40cfdcb9a5f388d5ecbae88be3141d434e7b90
yeswanth <yeshureddy1999@gmail.comgit config --global user.name yeswanthgit pushgit config --global user.email yeshureddy1999@gmail.com>
2024-09-28 20:15:43 +0530

    added

--- 6.condition.sh
+++ 6.condition.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-#NUMBER = $1
+NUMBER=$2
 
-#if ($NUMBER -gt 100)
- #   echo "Number is greater than 100"
-#else
-   # echo "Number is less than 100"
-#fi
+if  [ $NUMBER -gt 100 ]
+then
+    echo "Number is greater than 100"
+else
+    echo "Number is less than 100"
+fi
 
 #!/bin/bash
 
