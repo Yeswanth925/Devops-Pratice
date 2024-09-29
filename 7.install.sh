df7c6a9c1fcd61e7a9a3578ab394cf2123ba71b7
yeswanth <yeshureddy1999@gmail.comgit config --global user.name yeswanthgit pushgit config --global user.email yeshureddy1999@gmail.com>
2024-09-29 08:24:27 +0530

    added

--- 7.install.sh
+++ 7.install.sh
@@ -4,14 +4,13 @@
 
 if [$ID -ne 0]
 then
-    {
+    
         echo "error: please go with the root user"
-    }
-    exit 1
+        exit 1
 else
-    {
+    
         echo "good to proceed"
-    }
+    
 fi
 
 yum install mysql -y
