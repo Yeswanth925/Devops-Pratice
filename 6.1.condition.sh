fdfe4f809870ca708d3c8dac11c7b81f1609cff3
yeswanth <yeshureddy1999@gmail.comgit config --global user.name yeswanthgit pushgit config --global user.email yeshureddy1999@gmail.com>
2024-09-29 08:02:11 +0530

    added

--- 6.1.condition.sh
+++ 6.1.condition.sh
@@ -4,7 +4,11 @@ NUMBER=$20
 
 if [ $NUMBER -gt 100 ]
 then 
-    {echo "print the number $NUMBER  is greater than 100"}
+    {
+        echo "print the number $NUMBER  is greater than 100"
+        }
 else
-    {echo "print the number $NUMBER is less than 100"}
+    {
+        echo "print the number $NUMBER is less than 100"
+        }
 fi
